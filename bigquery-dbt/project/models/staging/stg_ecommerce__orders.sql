f66577df38ee8e599df3ae58a6d2d0fccd74fa29
Piotr <pmackowka@gmail.com>
2025-03-31 19:03:32 +0200

    project/models/staging/stg_ecommerce__order_items

--- bigquery-dbt/project/models/staging/stg_ecommerce__orders.sql
+++ bigquery-dbt/project/models/staging/stg_ecommerce__orders.sql
@@ -1,19 +1,22 @@
-
 WITH source AS (
-        SELECT *
+	SELECT *
 
-        FROM {{ source('thelook_ecommerce', 'orders') }}
+	FROM {{ source('thelook_ecommerce', 'orders') }}
 )
 
 SELECT
-        order_id,
-        user_id,
-        status,
-        gender,
-        created_at,
-        returned_at,
-        shipped_at,
-        delivered_at,
-        num_of_item
+	-- IDs
+	order_id,
+	user_id,
+
+	-- Timestamps
+	created_at,
+	returned_at,
+	delivered_at,
+	shipped_at,
+
+	-- Other columns
+	status,
+	num_of_item AS num_items_ordered
 
 FROM source
