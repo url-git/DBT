4dc185c655dc413eea3b46d659d8f52b3210bab5
Piotr <pmackowka@gmail.com>
2025-04-06 12:50:32 +0200

    project/models/staging/stg_ecommerce__events

--- bigquery-dbt/project/models/staging/stg_ecommerce__events.sql
+++ bigquery-dbt/project/models/staging/stg_ecommerce__events.sql
@@ -2,7 +2,12 @@
 	config(
 		materialized='incremental',
 		unique_key='event_id',
-		on_schema_change='sync_all_columns'
+		on_schema_change='sync_all_columns',
+		partition_by={
+			"field": "created_at",
+			"data_type": "timestamp",
+			"granularity": "day"
+		}
 	)
 }}
 
