770a253c6725348b4d255f4e7af7e4550caca170
Piotr <pmackowka@gmail.com>
2025-04-20 17:52:41 +0200

    macros/positive_value

--- snowflake-dbt/macros/positive_value.sql
+++ snowflake-dbt/macros/positive_value.sql
@@ -7,5 +7,4 @@ WHERE
     {{ column_name}} < 1
 {% endtest %}
 
-{# Powyższe makro "positive_value" wielokrotnego użytku to refaktor dla models/tests/dim_listings_minumum_nights.sql,
-które moze zostać usunięte #}
+{# Powyższe makro "positive_value" wielokrotnego użytku to refaktor dla models/tests/dim_listings_minumum_nights.sql, które moze zostać usunięte #}
